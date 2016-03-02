f0189fd32719e4d183a62eed1c414037363323d4
Nasser Brake <nasser.brake@gmail.com>
2016-03-02 12:08:14 +0100

    Verschachtelt, entschachtelt, und eine Computation expression

--- 20151202Extended/Option.fsx
+++ 20151202Extended/Option.fsx
@@ -82,3 +82,67 @@ isLarger (None) 1
 
 
 // These are just basic operations but they are consistent with the other operations in the other standard modules
+// Say you want to parse a string as int, proof that it's even, then add ten to it
+
+let ``add ten interleaved`` numberAsText = 
+    match Int32.TryParse(numberAsText) with // TryParse return a tuple!
+    | false, _ -> None 
+    | _, value -> match (value % 2 = 0) with
+                  | false -> None
+                  | true -> value + 10 |> Some  
+                  
+let inline parse< ^T when ^T : (static member TryParse : string * byref< ^T > -> bool) and  ^T : (new : unit -> ^T) > valueToParse =
+    let mutable output = new ^T()
+    let parsed = ( ^T : (static member TryParse  : string * byref< ^T > -> bool ) (valueToParse, &output) )
+    match parsed with
+    | true -> output |> Some
+    | _ -> None
+
+/// Note that I don't need to include logic pertaining to the None branch
+/// This is handled implicitly    
+let ``add ten using module functions`` numberAsText =
+    let parsed = parse<int> numberAsText
+    let even = Option.filter (fun x -> x % 2 = 0) parsed
+    let added = Option.bind (fun x -> x + 10 |> Some ) even
+    added
+
+// Option is great coupled with computation expressions
+open System
+
+/// Define a computation expression
+type MaybeBuilder() =
+    member __.Bind(option, continuation) = Option.bind continuation option
+    member __.Return(value) = Some value
+
+/// Create an instance of the builder
+let maybe = MaybeBuilder()
+
+// Now use the builder in a real function!
+
+/// Takes in a string version of a number, tries to convert it to an int, adds 10, and returns the string representation back out
+let addTen numberAsText =
+    maybe {
+        let! x =
+            match Int32.TryParse(numberAsText) with // TryParse return a tuple!
+            | false, _ -> None 
+            | _, value -> Some value
+        printfn "You provided me a convertable number!"
+        
+        let! y = 
+            match (x % 2 = 0) with
+            | true -> Some x
+            | false -> None
+        printfn "You provided me an even number, gonna add ten for you!"
+        
+        return x + 10
+    }
+
+/// Gets back a string option
+let values = ["15"; "n/a"; "12"; ]
+values |> List.map ``add ten interleaved``
+values |> List.map ``add ten using module functions``
+values |> List.map addTen
+
+values |> List.map addTen |> List.filter Option.isNone |> List.length
+
+
