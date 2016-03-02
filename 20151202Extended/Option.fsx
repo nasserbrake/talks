3e6f3b64f653cdac75b269b5b59a5b0fdf4eb3bd
Nasser Brake <nasser.brake@gmail.com>
2016-03-02 10:12:07 +0100

    Beispiele für die standard Funktionen aus dem Module Option

--- 20151202Extended/Option.fsx
+++ 20151202Extended/Option.fsx
@@ -1,9 +1,9 @@
 ﻿open System
 // Option wird vielfältig in funktionalen Sprachen verwendet
 // Deklaration
-type Option<'a> =   // use a generic definition 
-| Some of 'a        // valid value 
-| None              // missing
+//type Option<'a> =   // use a generic definition 
+//| Some of 'a        // valid value 
+//| None              // missing
 
 type SomeString = string option
 
@@ -43,3 +43,42 @@ let parseResult' = parseInt "Keine Zahl"
 
 showParseResult parseResult
 showParseResult parseResult'
+
+// Option module offers a host of useful functions
+// bind: (f:'T -> 'U option) -> 'T option -> 'U option
+// Take a function that can map a value fo 'T into a value of U'
+// Provide a value of 'T
+// Expect either None of 'U option
+// A possible implementation!
+// let bind (f:'T -> Option<'U>) (x:Option<'T>) : Option<'U> = 
+//     match x with    
+//     | Some v -> f v
+//     | None -> None
+//     
+let optionBindFun x = Some (sprintf "asdfasd %i" x)
+let v = Some(1)
+let bindStringOption = Option.bind optionBindFun v 
+
+// exists (f:'T -> bool)  -> 'T option -> bool
+// let exists f opt = 
+//     match opt with
+//     | None -> false
+//     | Some x -> f x
+let isValue opt value = Option.exists (fun elem -> elem = value) opt
+isValue (Some(2)) 2
+isValue (Some(2)) 3
+isValue (None) 3
+
+// map: (f:'T -> 'U) -> 'T option -> 'U option
+// let map f opt = 
+//     match opt with
+//     | None -> None
+//     | Some v -> Some(f v)
+
+// filter: (f:'T -> bool) -> 'T option -> 'T option
+let isLarger opt value = Option.filter (fun elem -> elem > value) opt
+isLarger (Some(2)) 1
+isLarger (None) 1
+
+
+// These are just basic operations but they are consistent with the other operations in the other standard modules
